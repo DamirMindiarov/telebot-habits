e80163c7d1506597ae1b2f1ceacab54b5b9f41f9
DamirMindiarov <akacuki1263547@gmail.com>
2025-06-20 13:50:33 +0700

    format list habits

--- bot/handlers/cb_show_habits/message.py
+++ bot/handlers/cb_show_habits/message.py
@@ -1,40 +1,35 @@
+import ast
+
 import requests
-from sqlalchemy import select
 from telebot.types import CallbackQuery
 
-from app.database import session_async, HabitsDB, UsersDB
 from bot.functions import get_token_by_user_id
-from loader import bot
 from bot.handlers.cb_show_habits.keyboard import keyboard
+from loader import bot
 
 
 @bot.callback_query_handler(
     func=lambda callback: callback.data == "cb_show_habits"
 )
 async def show_habits(callback: CallbackQuery):
-    # Запрос к бд на список привычек
-    # async with session_async() as session:
-    #     habits = await session.execute(select(HabitsDB).where(UsersDB.id == callback.from_user.id))
     token = await get_token_by_user_id(user_id=str(callback.from_user.id))
     headers = {"Authorization": f"Bearer {token}"}
 
-    response = requests.get(url="http://localhost:8000/habits", headers=headers)
-    # habits = """Список привычек на сегодня:
-    # Привычка_1
-    # Выполнена: 0 из 21
-    # Не выполнена: 0 из 21
-    # Изменить: /update_1
-    # Удалить: /delete_1
-    #
-    # Привычка_2
-    # Выполнена: 0 из 21
-    # Не выполнена: 0 из 21
-    # Изменить: /update_2
-    # Удалить: /delete_2
-    # """
-    habits = response.text
+    response = requests.get(url="http://localhost:8000/habits",
+                            headers=headers)
+    habits = ast.literal_eval(response.text)
+
+    sample_habit = """{name}\nВыполнена: {count_done} из 21\nИзменить: /update{id_habit}\nУдалить: /delete{id_habit}"""
+    list_habits = "Список привычек:\n"
+
+    for habit in habits:
+        list_habits += sample_habit.format(name=habit["name"],
+                                           count_done=habit["count_done"],
+                                           id_habit=habit["id"])
+        list_habits += "\n\n"
+
     await bot.send_message(
         chat_id=callback.from_user.id,
-        text=habits,
+        text=list_habits,
         reply_markup=keyboard,
     )
