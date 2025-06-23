beb46fb89ccbc1156a7a72cbd4d21fcc1c11ac12
DamirMindiarov <akacuki1263547@gmail.com>
2025-06-23 12:05:37 +0700

    add func check_auth

--- bot/functions.py
+++ bot/functions.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
 
 from app.database import session_async, UsersDB, HabitsDB
+from authorization.functions import get_current_user, token_is_alive
 
 
 async def get_token_by_user_id(user_id: str) -> str:
@@ -9,5 +10,11 @@ async def get_token_by_user_id(user_id: str) -> str:
             select(UsersDB.token).where(UsersDB.user_id == user_id))
         res = res.scalar()
         return res
+
+
+async def check_auth(user_id: int):
+    token = await get_token_by_user_id(user_id=str(user_id))
+    response = await token_is_alive(token=token)
+    return token if response else False
 
 
