5b3173e96460fb141d1e2fb2f75133da042c23eb
DamirMindiarov <akacuki1263547@gmail.com>
2025-06-23 12:04:13 +0700

    add func update_habit

--- app/functions.py
+++ app/functions.py
@@ -1,20 +1,22 @@
-from sqlalchemy import select, delete
+from sqlalchemy import select, delete, update
+from sqlalchemy.orm import defer
 
 from database import session_async, UsersDB, HabitsDB
 
 
 async def get_habits_by_user_id(user_id: str) -> str:
-
     async with session_async() as session:
-        habits = await session.execute(select(HabitsDB).where(UsersDB.user_id == user_id))
+        habits = await session.execute(
+            select(HabitsDB).where(UsersDB.user_id == user_id))
 
     return habits.scalars().fetchall()
 
 
-async def add_habit(name: str, count_done:int, user_id: str) -> HabitsDB:
-    habit: HabitsDB = HabitsDB(name=name, count_done=count_done, user_id=user_id)
+async def add_habit(name: str, count_done: int, user_id: str) -> HabitsDB:
+    habit: HabitsDB = HabitsDB(name=name, count_done=count_done,
+                               user_id=user_id)
 
-    async with session_async() as  session:
+    async with session_async() as session:
         session.add(habit)
         await session.commit()
 
@@ -22,6 +24,16 @@ async def add_habit(name: str, count_done:int, user_id: str) -> HabitsDB:
 
 
 async def del_habit(habit_id: int):
-    async with session_async() as  session:
+    async with session_async() as session:
         await session.execute(delete(HabitsDB).where(HabitsDB.id == habit_id))
+        await session.commit()
+
+
+async def update_habit(habit_id: int, habit_new_name: str):
+    async with session_async() as session:
+        await session.execute(
+            update(HabitsDB)
+            .where(HabitsDB.id == habit_id)
+            .values(name=habit_new_name)
+        )
         await session.commit()
