61008d2b2c80039c31764c460c90cb4f4e78a5f2
DamirMindiarov <akacuki1263547@gmail.com>
2025-06-23 12:06:32 +0700

    add state_storage into bot

--- bot/loader.py
+++ bot/loader.py
@@ -1,4 +1,6 @@
 from telebot.async_telebot import AsyncTeleBot
+from telebot.asyncio_storage import StateMemoryStorage
+
 from config import TOKEN
 
-bot = AsyncTeleBot(TOKEN)
+bot = AsyncTeleBot(token=TOKEN, state_storage=StateMemoryStorage())
--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,10 @@
 import asyncio
-
+import handlers
 from telebot import asyncio_filters
 
 from loader import bot
-import handlers
 
 if __name__ == '__main__':
-
     # Add custom filters
     bot.add_custom_filter(asyncio_filters.StateFilter(bot))
     # necessary for state parameter in handlers.
