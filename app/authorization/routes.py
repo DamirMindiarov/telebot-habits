079fbdfd4f756b6b55ac3df87fa50fd0168337f5
DamirMindiarov <akacuki1263547@gmail.com>
2025-07-02 14:41:54 +0700

    formated black app/routes.py

--- app/authorization/routes.py
+++ app/authorization/routes.py
@@ -1,24 +1,29 @@
 from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.authorization.functions import authenticate_user, refresh_token, \
-    add_user, get_current_user
 from app.authorization.config import pwd_context
-from authorization.pydentic_models import User
-from database import session_async
+from app.authorization.functions import (
+    authenticate_user,
+    refresh_token,
+    add_user,
+)
+from database import session_async, UsersDB
 
 router = APIRouter()
 
 
 @router.post("/token")
 async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """Добавляет токен в БД (Авторизация пользователя)"""
     async with session_async() as session:
-        user = await authenticate_user(form_data.username, form_data.password, session)
+        user = await authenticate_user(
+            form_data.username, form_data.password, session
+        )
         if not user:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"}
+                headers={"WWW-Authenticate": "Bearer"},
             )
 
         await refresh_token(user_id=user.user_id, session=session)
@@ -28,7 +33,11 @@ async def get_token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 
 @router.post("/registration", status_code=status.HTTP_201_CREATED)
-async def registration_user(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = await add_user(form_data.username,
-                          pwd_context.hash(form_data.password))
+async def registration_user(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+) -> UsersDB:
+    """Регистрирует нового пользователя"""
+    user = await add_user(
+        form_data.username, pwd_context.hash(form_data.password)
+    )
     return user
