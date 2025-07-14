88a59011452d4c24fb95e27d9ac0c7d15eb9fe26
DamirMindiarov <akacuki1263547@gmail.com>
2025-07-14 14:45:32 +0700

    refactor main.py start from main() func

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from telebot import asyncio_filters
 
 from loader import bot
 
-if __name__ == "__main__":
+async def main():
     # Add custom filters
     bot.add_custom_filter(asyncio_filters.StateFilter(bot))
     # necessary for state parameter in handlers.
     from telebot.states.asyncio.middleware import StateMiddleware
 
     bot.setup_middleware(StateMiddleware(bot))
-    bot.set_my_commands([BotCommand('help', 'помощь')])
+    await bot.set_my_commands([BotCommand('help', 'помощь')])
+    await bot.polling()
 
-    asyncio.run(bot.polling())
+if __name__ == "__main__":
+    asyncio.run(main())
